18c397fae81dcc1c624b3228a91051242710c73c
Ahmet Inan <xdsopl@gmail.com>
2020-01-17 17:13:57 +0100

    use new and simpler non-uniform spline interface

--- hermite2.cc
+++ hermite2.cc
@@ -9,35 +9,29 @@ Copyright 2020 Ahmet Inan <inan@aicodix.de>
 #include <functional>
 #include "spline.hh"
 #include "complex.hh"
-#include "utils.hh"
 
 int main()
 {
 	const int SAMPLES = 16;
-	const int FACT = 10;
 	typedef float value;
+	const value STEP = 0.05;
 	typedef DSP::Complex<value> cmplx;
 	typedef DSP::CubicHermiteSpline<cmplx, value> hermite;
-	value x[SAMPLES];
-	cmplx y[SAMPLES];
+	value X[SAMPLES];
+	cmplx Y[SAMPLES];
 	std::default_random_engine generator(42);
 	typedef std::uniform_real_distribution<value> distribution;
 	auto noise = std::bind(distribution(-1, 1), generator);
 	for (int i = 0; i < SAMPLES; ++i) {
-		y[i] = cmplx(noise(), noise());;
-		x[i] = i ? x[i-1]+abs(y[i]-y[i-1]) : value(0);
+		Y[i] = cmplx(noise(), noise());;
+		X[i] = i ? X[i-1]+abs(Y[i]-Y[i-1]) : value(0);
 	}
 	for (int i = 0; i < SAMPLES; ++i)
-		printf("%g %g %g NaN NaN\n", x[i], y[i].real(), y[i].imag());
-	for (int k = 0; k < SAMPLES-1; ++k) {
-		for (int i = 0; i < FACT; ++i) {
-			value t = value(i) / value(FACT);
-			cmplx tmp = hermite::eval(x, y, t, k, SAMPLES);
-			printf("%g NaN NaN %g %g\n", DSP::lerp(t, x[k], x[k+1]), tmp.real(), tmp.imag());
-		}
+		printf("%g %g %g NaN NaN\n", X[i], Y[i].real(), Y[i].imag());
+	for (value x = X[0]; x <= X[SAMPLES-1]; x += STEP) {
+		cmplx tmp = hermite::eval(X, Y, x, SAMPLES);
+		printf("%g NaN NaN %g %g\n", x, tmp.real(), tmp.imag());
 	}
-	cmplx tmp = hermite::eval(x, y, value(1), SAMPLES-2, SAMPLES);
-	printf("%g NaN NaN %g %g\n", x[SAMPLES-1], tmp.real(), tmp.imag());
 	return 0;
 }
 
