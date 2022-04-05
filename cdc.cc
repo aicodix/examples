da8359190006342ed07807ab474f05c28e8864bc
Ahmet Inan <xdsopl@gmail.com>
2022-04-05 12:00:08 +0200

    added option to output polar form for cdc too

--- cdc.cc
+++ cdc.cc
@@ -12,13 +12,15 @@ Copyright 2020 Ahmet Inan <inan@aicodix.de>
 
 int main(int argc, char **argv)
 {
-	if (argc != 3) {
-		std::cerr << "usage: " << argv[0] << " OUTPUT INPUT" << std::endl;
+	if (argc < 3 || argc > 4) {
+		std::cerr << "usage: " << argv[0] << " OUTPUT INPUT [COMPLEX|POLAR]" << std::endl;
 		return 1;
 	}
 
 	const char *out_name = argv[1];
 	const char *inp_name = argv[2];
+
+	bool polar = argc == 4 && strcmp("POLAR", argv[3]) == 0;
 
 	typedef float value;
 	typedef DSP::Complex<value> cmplx;
@@ -44,6 +46,11 @@ int main(int argc, char **argv)
 		value input[2];
 		inp_file.read(input, 2);
 		cmplx output = cdc(input[0], input[1]);
+		if (polar) {
+			value mag = abs(output);
+			value ang = arg(output) / DSP::Const<value>::Pi();
+			output = cmplx(mag, ang);
+		}
 		out_file.write(reinterpret_cast<value *>(&output), 1);
 	}
 
