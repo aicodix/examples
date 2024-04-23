3729acc72c312ef5e562987fa3ddfcb8c8bb43db
Ahmet Inan <xdsopl@gmail.com>
2024-04-23 11:00:58 +0200

    added option to get instantaneous frequency

--- cdc.cc
+++ cdc.cc
@@ -8,19 +8,26 @@ Copyright 2020 Ahmet Inan <inan@aicodix.de>
 #include <cmath>
 #include "complex.hh"
 #include "cdc.hh"
+#include "fmd.hh"
 #include "wav.hh"
 
 int main(int argc, char **argv)
 {
 	if (argc < 3 || argc > 4) {
-		std::cerr << "usage: " << argv[0] << " OUTPUT INPUT [COMPLEX|POLAR]" << std::endl;
+		std::cerr << "usage: " << argv[0] << " OUTPUT INPUT [COMPLEX|POLAR|FREQ]" << std::endl;
 		return 1;
 	}
 
 	const char *out_name = argv[1];
 	const char *inp_name = argv[2];
 
-	bool polar = argc == 4 && strcmp("POLAR", argv[3]) == 0;
+	int mode = 0;
+	if (argc == 4) {
+		if (strcmp("POLAR", argv[3]) == 0)
+			mode = 1;
+		if (strcmp("FREQ", argv[3]) == 0)
+			mode = 2;
+	}
 
 	typedef float value;
 	typedef DSP::Complex<value> cmplx;
@@ -41,14 +48,20 @@ int main(int argc, char **argv)
 
 	const int TAPS = 129;
 	DSP::CDC<cmplx, TAPS> cdc;
+	DSP::FMD1<cmplx> demod;
+	demod.bandwidth(1);
 
 	while (out_file.good() && inp_file.good()) {
 		value input[2];
 		inp_file.read(input, 2);
 		cmplx output = cdc(input[0], input[1]);
-		if (polar) {
+		if (mode > 0) {
 			value mag = abs(output);
-			value ang = arg(output) / DSP::Const<value>::Pi();
+			value ang;
+			if (mode == 1)
+				ang = arg(output) / DSP::Const<value>::Pi();
+			else
+				ang = demod(output);
 			output = cmplx(mag, ang);
 		}
 		out_file.write(reinterpret_cast<value *>(&output), 1);
